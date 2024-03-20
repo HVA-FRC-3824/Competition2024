b6cf6938fbc1c6c8ca1035d37692d4bee30630f0
Anorak803 <s945414@student.knoxschools.org>
2024-03-20 19:29:43 -0400

    Updated Robot.cpp to attempt a push.

--- Competition2024/src/main/cpp/Robot.cpp
+++ Competition2024/src/main/cpp/Robot.cpp
@@ -5,50 +5,49 @@
 #include "Robot.h"
 
 #include <fmt/core.h>
-#include <frc/smartdashboard/SmartDashboard.h>
 #include <frc/Joystick.h>
 #include <frc/smartdashboard/SendableChooser.h>
+#include <frc/smartdashboard/SmartDashboard.h>
 #include <iostream>
 
 #include "../include/io/OperatorController.hpp"
 #include "../include/io/CommandHelper.hpp"
 #include "../include/io/DriverController.hpp"
+
 #include "../include/subsystems/Launcher.hpp"
 #include "../include/subsystems/Swerve.hpp"
 #include "../include/subsystems/Autos.hpp"
 #include "../include/subsystems/Climb.hpp"
+
 #include "../include/Memory.h"
 
 /* Object and struct declaration */
-cmd_share       cmd_control;
-angle_mem_share angles_share;
-Launcher        LAUNCHER{};
-Intake          INTAKE{};
-Actuation       ACTUATION{};
-Climb           CLIMB{};
+cmd_share          cmd_control;
+Launcher           LAUNCHER{};
+Intake             INTAKE{};
+Actuation          ACTUATION{};
+Climb              CLIMB{};
+Swerve             SWERVE{24, 24};
 
-Swerve SWERVE{24,24};
-
-OperatorController O_CONTROLLER{&cmd_control,&LAUNCHER,&INTAKE,&ACTUATION, &CLIMB}; 
+OperatorController O_CONTROLLER{&cmd_control, &LAUNCHER, &INTAKE, &ACTUATION, &CLIMB};
 DriverController   D_CONTROLLER{&SWERVE};
 
-Autos AUTOS{&SWERVE,&cmd_control,&ACTUATION};
+Autos              AUTOS{&SWERVE, &cmd_control, &ACTUATION};
 
-frc::Joystick Jostick{0};
-uint8_t blocked_tags[8] = {8,9,10,11,12,13,14,15};
+frc::Joystick      Jostick{0};
 
-
-void Robot::RobotInit() {
+void Robot::RobotInit()
+{
   m_chooser.SetDefaultOption(kAutoNameCustom, kAutoNameCustom);
   m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
-  m_chooser.AddOption(kShootMoveAuto,kShootMoveAuto);
+  m_chooser.AddOption(kShootMoveAuto, kShootMoveAuto);
   m_chooser.AddOption(kMobilityAuto, kMobilityAuto);
   m_chooser.AddOption(kShootMoveLeftAuto, kShootMoveLeftAuto);
   m_chooser.AddOption(kShootMoveRightAuto, kShootMoveRightAuto);
   frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
-  //open_TheEye(THE_EYE,blocked_tags,8);
-  //navx.Reset();
-  runner_launcher(&cmd_control,&O_CONTROLLER,&AUTOS);
+  // open_TheEye(THE_EYE,blocked_tags,8);
+  // navx.Reset();
+  runner_launcher(&cmd_control, &O_CONTROLLER, &AUTOS);
 }
 
 /**
@@ -59,7 +58,9 @@ void Robot::RobotInit() {
  * <p> This runs after the mode specific periodic functions, but before
  * LiveWindow and SmartDashboard integrated updating.
  */
-void Robot::RobotPeriodic() { /*print_data(tag);*/ }
+void Robot::RobotPeriodic()
+{ /*print_data(tag);*/
+}
 
 /**
  * This autonomous (along with the chooser code above) shows how to select
@@ -72,32 +73,46 @@ void Robot::RobotPeriodic() { /*print_data(tag);*/ }
  * if-else structure below with additional strings. If using the SendableChooser
  * make sure to add them to the chooser code above as well.
  */
-void Robot::AutonomousInit() {
-  //SWERVE.snap_wheels_to_abs(); 
+void Robot::AutonomousInit()
+{
+  // SWERVE.snap_wheels_to_abs();
   m_autoSelected = m_chooser.GetSelected();
   // m_autoSelected = SmartDashboard::GetString("Auto Selector",
   //     kAutoNameDefault);
   fmt::print("Auto selected: {}\n", m_autoSelected);
 
-  if (m_autoSelected == kAutoNameCustom) {
+  if (m_autoSelected == kAutoNameCustom)
+  {
     AUTOS.do_nothing_auto();
-  } else if(m_autoSelected == kShootMoveAuto) {
+  }
+  else if (m_autoSelected == kShootMoveAuto)
+  {
     AUTOS.shoot_then_move();
-  } else if (m_autoSelected == kMobilityAuto) {
+  }
+  else if (m_autoSelected == kMobilityAuto)
+  {
     AUTOS.mobility();
-  } else if (m_autoSelected == kShootMoveLeftAuto) {
+  }
+  else if (m_autoSelected == kShootMoveLeftAuto)
+  {
     AUTOS.shoot_then_move_left();
-  } else if (m_autoSelected == kShootMoveRightAuto) {
+  }
+  else if (m_autoSelected == kShootMoveRightAuto)
+  {
     AUTOS.shoot_then_move_right();
   }
 }
 
-void Robot::AutonomousPeriodic() {
-  if(AUTOS.state == A_TRYING_FOR_ANGLE)
+void Robot::AutonomousPeriodic()
+{
+  if (AUTOS.state == A_TRYING_FOR_ANGLE)
   {
-    //SWERVE.drive(0,0,AUTOS.ANGLE_CONTROLLER.Calculate(SWERVE.navx.GetYaw()),0);
-    frc::SmartDashboard::PutNumber("ANGLE CONTROLLER OUTPUT: ",AUTOS.ANGLE_CONTROLLER.Calculate(SWERVE.navx.GetYaw()));
-    if(AUTOS.ANGLE_CONTROLLER.AtSetpoint()){AUTOS.state = A_FINISHED;}
+    // SWERVE.drive(0,0,AUTOS.ANGLE_CONTROLLER.Calculate(SWERVE.navx.GetYaw()),0);
+    frc::SmartDashboard::PutNumber("ANGLE CONTROLLER OUTPUT: ", AUTOS.ANGLE_CONTROLLER.Calculate(SWERVE.navx.GetYaw()));
+    if (AUTOS.ANGLE_CONTROLLER.AtSetpoint())
+    {
+      AUTOS.state = A_FINISHED;
+    }
   }
 }
 
@@ -109,18 +124,18 @@ void Robot::TeleopInit()
 
 int timer = 0;
 
-void Robot::TeleopPeriodic() {
+void Robot::TeleopPeriodic()
+{
   O_CONTROLLER.robo_periodic(); /* Operator Periodic */
   D_CONTROLLER.robo_periodic(); /* Driver Periodic   */
 
   command_runner();
   INTAKE.robo_periodic();
   ACTUATION.robo_periodic();
-  //angles_share.swerve_heading = navx.GetAngle();
-  //std::cout << navx.GetAngle() << "\n";
+  // std::cout << navx.GetAngle() << "\n";
 }
 
-void Robot::DisabledInit() { }
+void Robot::DisabledInit() {}
 
 void Robot::DisabledPeriodic() {}
 
@@ -133,7 +148,8 @@ void Robot::SimulationInit() {}
 void Robot::SimulationPeriodic() {}
 
 #ifndef RUNNING_FRC_TESTS
-int main() {
+int main()
+{
   return frc::StartRobot<Robot>();
 }
 #endif
